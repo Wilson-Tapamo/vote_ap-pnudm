ece5b8dce2c1a59d34b668b195df67366647335b
wilson Tapamo <capucinesbetsi@gmail.com>
2025-02-11 17:13:12 +0100

    change

--- frontend/pages/vote_president.vue
+++ frontend/pages/vote_president.vue
@@ -1,29 +1,90 @@
 <template>
-    <h1 class="font-bold text-3xl text-center">Bienvenue sur la plateforme de vote de l’AP-PNUDM</h1>
+       <h1 class="text-3xl text-center">Vote du <span class="font-bold">Président de l’AP-PNUDM</span></h1>
+    
+    <div class="flex justify-end hjy self-center w-full">
+      <button
+      type="submit"
+      class="py-2 px-4 bg-blue-500 buu text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+    >
+      Suivant
+    </button>
+    </div>
+<div class="max-w-screen-xl m-0 sm:m-6 bg-white shadow try flex  flex-1 self-center w-full shadow-xl">
+<div class="max-w-xl  bg-white p-8  rounded-lg">
+  <h1 class="yui">Sélectionnez votre candidat puis, cliquez sur suivant.</h1>
+  <form @submit.prevent="submitVote">
+    <div class="space-y-4">
+      <div class="flex items-center">
+          <span class="circle" style="background-image: url(img3.jpg);background-position: center;object-fit: cover;background-size: cover;"></span>
+        <label for="candidate1" class="ml-2 text-lg lab">Djeneba Diawara</label>
+        <input
+          v-model="selectedCandidate"
+          type="radio"
+          id="candidate1"
+          value="candidate1"
+          name="president"
+          class="h-5 w-5 text-red-900 ip"
+        />
+        
+      </div>
+    </div>
+    <div class="space-y-4">
+      <div class="flex items-center">
+          <span class="circle" style="background-image: url(img2.jpg);background-position: center;object-fit: cover;background-size: cover;"></span>
+        <label for="candidate1" class="ml-2 text-lg lab">Faran Sidibe</label>
+        <input
+          v-model="selectedCandidate"
+          type="radio"
+          id="candidate1"
+          value="candidate1"
+          name="president"
+          class="h-5 w-5 text-red-900 ip"
+        />
+        
+      </div>
+    </div>
+    <div class="space-y-4">
+      <div class="flex items-center">
+          <span class="circle" style="background-image: url(img1.jpg);background-position: center;object-fit: cover;background-size: cover;"></span>
+        <label for="candidate1" class="ml-2 text-lg lab">Moussa Diomande</label>
+        <input
+          v-model="selectedCandidate"
+          type="radio"
+          id="candidate1"
+          value="candidate1"
+          name="president"
+          class="h-5 w-5 text-red-900 ip"
+        />
+        
+      </div>
+    </div>
+    <div class="space-y-4">
+      <div class="flex items-center">
+          <span class="circle" style="background-color: white;background-position: center;object-fit: cover;background-size: cover;"></span>
+        <label for="candidate1" class="ml-2 text-lg lab">Vote Blanc</label>
+        <input
+          v-model="selectedCandidate"
+          type="radio"
+          id="candidate1"
+          value="candidate1"
+          name="president"
+          class="h-5 w-5 text-red-900 ip"
+        />
+        
+      </div>
+    </div>
 
-    <div class="max-w-screen-xl m-0 sm:m-10 bg-white shadow  flex  flex-1 self-center w-full shadow-xl">
-      <div class="max-w-xl m-2 bg-white p-8  rounded-lg">
-        <h1 class="">Sélectionnez votre candidat puis, cliquez sur suivant.</h1>
-        <form @submit.prevent="submitVote">
-      <div>
-        <input type="radio" id="Djeneba" name="vote" value="Djeneba_Diawara" v-model="selectedCandidate" />
-        <label for="Djeneba">Djeneba Diawara</label>
-      </div>
-      <div>
-        <input type="radio" id="Faran" name="vote" value="Faran_Sidibe" v-model="selectedCandidate" />
-        <label for="Faran">Faran Sidibe</label>
-      </div>
-      <div>
-        <input type="radio" id="Moussa" name="vote" value="Moussa_Diomande" v-model="selectedCandidate" />
-        <label for="Moussa">Moussa Diomande</label>
-      </div>
-      <div>
-        <input type="radio" id="Blanc" name="vote" value="Vote_Blanc" v-model="selectedCandidate" />
-        <label for="Blanc">Vote Blanc</label>
-      </div>
-      <button type="submit">Suivant</button>
-        </form>
-      </div>
+  </form>
+</div>
+</div>
+<div class="flex justify-between hjy self-center w-full">
+  <span class="text-white pr-8">Votant : janvier.akowo@undp.org </span>
+      <button
+      type="submit"
+      class="py-2 px-4 bg-blue-500 buu text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+    >
+      Suivant
+    </button>
     </div>
   </template>
   
