37bebb7855408e38fc68ddcfa1ab34ec9e191ac0
wilson Tapamo <capucinesbetsi@gmail.com>
2025-02-14 18:07:12 +0100

    Delete backend directory

--- backend/dist/vote-president/vote-president.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { VotePresidentService } from './vote-president.service';
-export declare class VotePresidentController {
-    private readonly votePresidentService;
-    constructor(votePresidentService: VotePresidentService);
-    vote(body: {
-        login_email: string;
-        candidate: string;
-    }): Promise<any>;
-}
--- backend/dist/vote-president/vote-president.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Repository } from 'typeorm';
-import { VotePresident } from './vote-president.entity';
-export declare class VotePresidentService {
-    private votePresidentRepository;
-    constructor(votePresidentRepository: Repository<VotePresident>);
-    updateVote(login_email: string, candidate: string): Promise<any>;
-}
